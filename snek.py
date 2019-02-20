f4f5938fc2d5f9e7a91bc1efa7a2da1896fe0e3f
Temebe <seboado@gmail.com>
2019-02-20 10:21:40 +0100

    Small changes, repaired bug

--- snek.py
+++ snek.py
@@ -5,6 +5,9 @@ fruitImg = pygame.image.load("res/fruit.png")
 bodyImg = pygame.image.load("res/body.png")
 bgImg = pygame.image.load("res/background.png")
 mapSize = 20
+squareSize = 20
+gapSize = 5
+
 screenSize = 1200, 800
 black = 0, 0, 0
 
@@ -12,23 +15,27 @@ class SquareType(Enum):
     BODY = 0
     FRUIT = 1
     BACKGROUND = 2
+    
 class Snake:
     pass
 
 class Square:
 
-    def __init__(self, posX, posY):
+    def __init__(self, cordX, cordY):
         self.img = bgImg
         self.type = SquareType.BACKGROUND
-        self.posX = posX
-        self.posY = posY
-        self.pos = posX, posY
+        self.posX = cordX * (squareSize + gapSize)
+        self.posY = cordY * (squareSize + gapSize)
+        self.pos = self.posX, self.posY
+        self.cordX = cordX
+        self.cordY = cordY
+        self.cords = self.cordX, self.cordY
         pass
 
     def changeType(self, squareType):
         if self.type == squareType: return
         self.type = squareType
-        if squareType == SquareType.BACKGROUND: self.img = bgImg
+        if squareType == SquareType.BACKGROUND: self.img = bgImgx
         if squareType == SquareType.BODY: self.img = bodyImg
         if squareType == SquareType.FRUIT: self.img = fruitImg
 
@@ -37,7 +44,7 @@ class Map:
     def __init__(self):
         self.squares = dict()
         for i, j in list([(i, j) for i in range(mapSize) for j in range(mapSize)]):
-            self.squares[(i, j)] = Square(i * 25, j * 25)
+            self.squares[(i, j)] = Square(i, j)
 
 
 pygame.init()
@@ -52,7 +59,9 @@ while True:
             posX, posY = pygame.mouse.get_pos()
             i = int(posX/25)
             j = int(posY/25)
+            if i >= mapSize or j >= mapSize: break
             map.squares[(i, j)].changeType(SquareType.BODY)
+            print("Clicked ({0}, {1}) ".format(i, j))
     
     screen.fill(black)
     for square in map.squares.values():
