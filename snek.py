a4c470ed7c5541fc35a647d9582bb60920e27078
Temebe <seboado@gmail.com>
2019-02-25 00:34:47 +0100

    now snake has a queued body, i can also make fruits. Divided project into smaller files

--- snek.py
+++ snek.py
@@ -1,67 +1,37 @@
 import pygame, sys
-from enum import Enum
-
-fruitImg = pygame.image.load("res/fruit.png")
-bodyImg = pygame.image.load("res/body.png")
-bgImg = pygame.image.load("res/background.png")
-mapSize = 20
-squareSize = 20
-gapSize = 5
+import snake, square, map as mapClass, enums
 
 screenSize = 1200, 800
 black = 0, 0, 0
 
-class SquareType(Enum):
-    BODY = 0
-    FRUIT = 1
-    BACKGROUND = 2
-    
-class Snake:
-    pass
-
-class Square:
-
-    def __init__(self, cordX, cordY):
-        self.img = bgImg
-        self.type = SquareType.BACKGROUND
-        self.posX = cordX * (squareSize + gapSize)
-        self.posY = cordY * (squareSize + gapSize)
-        self.pos = self.posX, self.posY
-        self.cordX = cordX
-        self.cordY = cordY
-        self.cords = self.cordX, self.cordY
-        pass
-
-    def changeType(self, squareType):
-        if self.type == squareType: return
-        self.type = squareType
-        if squareType == SquareType.BACKGROUND: self.img = bgImgx
-        if squareType == SquareType.BODY: self.img = bodyImg
-        if squareType == SquareType.FRUIT: self.img = fruitImg
-
-class Map:
-
-    def __init__(self):
-        self.squares = dict()
-        for i, j in list([(i, j) for i in range(mapSize) for j in range(mapSize)]):
-            self.squares[(i, j)] = Square(i, j)
+def clickedPos():
+    posX, posY = pygame.mouse.get_pos()
+    x = int(posX/25)
+    y = int(posY/25)
+    if x >= enums.mapSize or y >= enums.mapSize: return None
+    else: return (x, y)
 
 
 pygame.init()
 screen = pygame.display.set_mode(screenSize)
-map = Map()
+map = mapClass.Map()
+snake = snake.Snake(map.squares[(10, 10)])
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             #Edit this later
-            posX, posY = pygame.mouse.get_pos()
-            i = int(posX/25)
-            j = int(posY/25)
-            if i >= mapSize or j >= mapSize: break
-            map.squares[(i, j)].changeType(SquareType.BODY)
-            print("Clicked ({0}, {1}) ".format(i, j))
+            if clickedPos() != None: (x, y) = clickedPos()
+            else: break
+            #map.squares[(i, j)].changeType(SquareType.BODY)
+            snake.move(map.squares[(x, y)])
+            print("Clicked ({0}, {1}) ".format(x, y))
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+            if clickedPos() != None: (x, y) = clickedPos()
+            else: break
+            map.squares[(x, y)].changeType(enums.SquareType.FRUIT)
+            print("Fruited ({0}, {1}) ".format(x, y))
     
     screen.fill(black)
     for square in map.squares.values():
